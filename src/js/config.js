d172355a6777c9291ee60413e6abbf1329d62040
AdamJety <ajetmar@seznam.cz>
2022-09-24 21:29:09 +0200

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'a13d2008-13c7-4b52-ac8b-083a65020047';
+export const MODAL_CLOSE_SEC = 2.5;
